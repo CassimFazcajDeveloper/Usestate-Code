40f748a03d8699b1eebc75ef6075538d4239672e
CassimFazcajDeveloper <cassimmikochi@gmail.com>
2026-02-11 08:41:13 +0200

    sharing state between componets

--- useStatePractce/src/App.tsx
+++ useStatePractce/src/App.tsx
@@ -1,13 +1,20 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 
 
 function Demo() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
+  useEffect(() => {
+    console.log('The count is' + count)
+  
+    return () => {
+      console.log("its just a clean up console")
+    }
+  }, [count])
   return (
     <div>
-   <h1>count : {count}</h1>
+   <h1> Count : {count}</h1>
    <button onClick={()=> setCount(count + 1)}>Increment</button> <span> </span>
    <button onClick={()=> setCount(count - 1)}>Decrement</button>
     </div>
